b1eda34d2696e02ca58e27bcd5f4658aaf6d0f30
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-16 01:41:29 +0100

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+const CopyPlugin = require("copy-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin").default
+
+module.exports = {
+    entry: {
+        'bundle': [
+            path.resolve(__dirname, './source/main.js')
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "./palettes/", to: "./palettes/" },
+            ],
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                minify: TerserPlugin.uglifyJsMinify,
+                terserOptions: {
+                    compress: true
+                },
+                parallel: true,
+            })
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+}
