de151e2b763923983a3f927b587e9c2fca157f56
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-28 19:57:04 +0100

    Removed raymarch shader #1

--- source/shaders.js
+++ source/shaders.js
@@ -4,7 +4,6 @@ import discretebodies from './shaders/discretebodies.frag'
 import mellowbodies from './shaders/mellowbodies.frag'
 import noise from './shaders/noise.frag'
 import rainbowflower from './shaders/rainbowflower.frag'
-import raymarch from './shaders/raymarch.frag'
 import shapes from './shaders/shapes.frag'
 import smears from './shaders/smears.frag'
 import truchet from './shaders/truchet.frag'
@@ -21,7 +20,6 @@ export default {
         { name: 'mellowbodies', source: mellowbodies.sourceCode },
         { name: 'noise', source: noise.sourceCode },
         { name: 'rainbowflower', source: rainbowflower.sourceCode },
-        { name: 'raymarch', source: raymarch.sourceCode },
         { name: 'shapes', source: shapes.sourceCode },
         { name: 'smears', source: smears.sourceCode },
         { name: 'truchet', source: truchet.sourceCode },
